6ded9afe1ddac1938783914ac46501f48a482ac9
boat46450 <boat46450@hotmail.com>
2017-11-02 14:26:34 +0700

    add migrate payment & shipping & order

---
+++ database/migrations/2017_11_02_072145_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('customerId')->unsigned();
+            $table->integer('paymentId')->unsigned();
+            $table->integer('shippingId')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('customerId')->references('id')->on('customers');
+            $table->foreign('paymentId')->references('id')->on('payments');
+            $table->foreign('shippingId')->references('id')->on('shippings');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
