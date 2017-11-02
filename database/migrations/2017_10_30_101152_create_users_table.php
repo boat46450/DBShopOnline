00214f44fcde39928272845970e785b01bf4c7c1
boat46450 <boat46450@hotmail.com>
2017-11-02 13:58:15 +0700

    add migrate shop&userShop

--- database/migrations/2017_10_30_101152_create_users_table.php
+++ database/migrations/2017_10_30_101152_create_users_table.php
@@ -15,11 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('password');
-            $table->rememberToken();
+            $table->integer('customerId')->unsigned();
             $table->timestamps();
+
+            $table->foreign('customerId')->references('id')->on('customers');
         });
     }
 
--- database/migrations/2017_11_02_064659_create_shops_table.php
+++ database/migrations/2017_11_02_064659_create_shops_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCustomersTable extends Migration
+class CreateShopsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateCustomersTable extends Migration
      */
     public function up()
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('shops', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('surname');
+            $table->string('detail');
             $table->string('address');
             $table->string('city');
             $table->string('zipcode');
@@ -32,6 +32,6 @@ class CreateCustomersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('shops');
     }
 }
